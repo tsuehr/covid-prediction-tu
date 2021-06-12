2f377f263759d01e192faef7b61b2badcb233246
Anuar Santoyo Alum <anuarsantoyo@hotmail.com>
2021-06-12 11:33:14 +0200

    nll

--- NLL_debug.py
+++ NLL_debug.py
@@ -1,3 +1,5 @@
+# %%
+
 import scipy.stats
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +12,6 @@ from torch import distributions
 from torch import rand
 from torch import autograd
 from torch import optim
-import pickle
 
 # %%
 
@@ -22,56 +23,90 @@ device = torch.device("cpu")
 
 # %%
 
-data = pd.read_csv('../../data/covid19model.csv')
+data = pd.read_csv('data/covid19model.csv')
+
+# %% md
+
+# Initialization
 
 
 # %%
 
-def trunc_exponential(scale, upper):
-    sample = torch.distributions.exponential.Exponential(1 / scale).rsample()
-    sample = sample / torch.tensor(1 - torch.exp(-upper / scale))
-    return sample
+cero = torch.tensor(0., requires_grad=False, device=device, dtype=dtype)
+num_impute = 6
+observed_daily_hospit = torch.tensor(data.hospit, requires_grad=False, device=device, dtype=dtype)
+pi = torch.tensor(data.delay_distr, requires_grad=False, device=device, dtype=dtype)
+serial_interval = torch.tensor(data.serial_interval, requires_grad=False, device=device, dtype=dtype)
+population = torch.tensor(5793636, requires_grad=False, device=device, dtype=dtype)
+num_observations = len(observed_daily_hospit)
 
-def trunc_normal(mu, sigma, under, upper):
-    distribution = torch.distributions.normal.Normal(loc=mu, scale=sigma, validate_args=None)
-    normal_sample = distribution.rsample()
-    cumulative = distribution.cdf(torch.tensor(upper)) - distribution.cdf(torch.tensor(under))
-    return normal_sample / cumulative
+# %% md
 
-def bij_transform(R0_prime, lower, upper):
+## Initialize latent variables/parameters
+
+# %%
+
+tau_prime = torch.tensor(np.random.exponential(1 / 0.03), requires_grad=True, device=device, dtype=dtype)
+phi_prime = torch.tensor(truncnorm.rvs((0 - 25) / 10, (np.inf - 25) / 10, loc=25, scale=10), requires_grad=True,
+                         device=device,
+                         dtype=dtype)  # has to be positive, between 0-50 --> uniform # dispersion (shape) parameter for observations
+R0_prime = torch.tensor(truncnorm.rvs((2 - 3.6) / 0.8, (5 - 3.6) / 0.8, loc=3.6, scale=0.8), requires_grad=True,
+                        device=device,
+                        dtype=dtype)  # probably gamma or inverse gamma distribution (compare to truncated normal) # initial reproduction number
+alpha_prime = torch.tensor(
+    truncnorm.rvs((0 - 1 / 100) / 1 / 100, (5 / 100 - 1 / 100) / 1 / 100, loc=1 / 100, scale=1 / 100),
+    requires_grad=True, device=device,
+    dtype=dtype)  # uniform distribution between (0-5%) # probability to get hospitalized
+sigma_prime = torch.tensor(truncnorm.rvs((0 - 0.1) / 0.3, (0.5 - 0.1) / 0.3, loc=0.1, scale=0.3), requires_grad=True,
+                           device=device,
+                           dtype=dtype)  # positive, tricky, gamma or inverse gamma, log normal  --> try something out, large sigma--> prone to overfitting # standart deviation of random walk step
+
+epsilon_t = torch.zeros(num_observations, device=device)
+epsilon_t[0] = torch.distributions.Normal(cero, sigma_prime.detach()).rsample()
+for t in range(1, num_observations):
+    epsilon_t[t] = torch.distributions.Normal(epsilon_t[t - 1].detach(), sigma_prime.detach()).rsample()
+epsilon_t.requires_grad_(True)
+
+
+# %% md
+
+# Define Forward Pass
+
+# %%
+
+def bij_transform(prime, lower, upper):
     # Recieves a value in [-inf, inf] and returns value in [low, upper]
-    bij = 1 / (1 + torch.exp(-R0_prime))
+    bij = 1 / (1 + torch.exp(-prime))
     scale = upper - lower
     return scale * bij + lower
+
 
 def bij_transform_inf(prime):
     return torch.exp(prime)
 
+
+# %%
+
 def calc_prior_loss(tau, phi, R0, alpha, sigma):
     # log likelihood wrt. our prior ("regularisation")
     # ll stands for log-likelihood
+    multiplier = 10
     ll = torch.tensor(0.0, device=device)
 
-    # dist_tau_t = distributions.exponential.Exponential(torch.tensor([1/0.03]))
-    # ll += dist_tau_t.log_prob(tau).item()
+    ll += (tau - (1 / 0.03)) ** 2 * multiplier
 
-    # dist_y = distributions.exponential.Exponential(tau) #the parameter in the brasket should either be float or tensor, to avoid any inconvienience,
-    # I use everything as tensor. NOTE:tau_t is already a tensor.
-    # ll += dist_y.log_prob(y).item()
+    ll += (phi - 25) ** 2 * multiplier
 
-    # dist_phi = distribution.normal.Normal(loc=torch.tensor([25]), scale=torch.tensor([10]))
-    ll += dist_phi.log_prob(phi).item()
+    ll += (R0 - 3.6) ** 2 * multiplier
 
-    # dist_R0 = distribution.normal.Normal(loc=torch.tensor([3.6]), scale=torch.tensor([0.8]))
-    ll += dist_R0.log_prob(R0).item()
+    ll += (alpha - 0.01) ** 2 * multiplier
 
-    # dist_alpha = distribution.normal.Normal(loc=torch.tensor([0.01]), scale=torch.tensor([0.01]))
-    ll += dist_alpha.log_prob(alpha).item()
-
-    # dist_sigma = distribution.normal.Normal(loc=torch.tensor([0.1]), scale=torch.tensor([0.3]))
-    ll += dist_sigma.log_prob(sigma).item()
+    ll += (sigma - 0.1) ** 2 * multiplier
 
     return ll
+
+
+# %%
 
 def seed_init_infect(y):
     # Initialize newly_infected, cumulative_infected, St
@@ -87,6 +122,9 @@ def seed_init_infect(y):
                                      cumulative_infected[0:num_impute].clone()])
     return newly_infected, cumulative_infected, St
 
+
+# %%
+
 def calc_Rt(R0, epsilon_t, sigma, ll):
     # Initialize eta_t
     eta_t = torch.zeros(num_observations, device=device)  # transformed reproduction number
@@ -97,10 +135,13 @@ def calc_Rt(R0, epsilon_t, sigma, ll):
     eta_t[0] = beta_0
     for t in range(1, num_observations):
         dist_epsilon_t = torch.distributions.Normal(epsilon_t[t - 1], sigma)
-        ll += dist_epsilon_t.log_prob(epsilon_t[t])
+        ll += dist_epsilon_t.log_prob(epsilon_t[t - 1])
     eta_t[1:num_observations] = beta_0 + epsilon_t[0:num_observations - 1].clone()
     Rt = torch.exp(eta_t)
     return Rt, ll
+
+
+# %%
 
 def calc_infections(cumulative_infected, newly_infected, St, Rt):
     # Initialize effectively_infectious
@@ -120,6 +161,9 @@ def calc_infections(cumulative_infected, newly_infected, St, Rt):
         newly_infected[t] = St[t].clone() * Rt[t].clone() * effectively_infectious[t].clone()
     return newly_infected
 
+
+# %%
+
 def calc_hospit(newly_infected, alpha):
     # Initialize expected_daily_hospit
     expected_daily_hospit = torch.zeros(num_observations, device=device)  # expected number of daily hospitalizations
@@ -133,6 +177,9 @@ def calc_hospit(newly_infected, alpha):
     expected_daily_hospit = alpha * expected_daily_hospit
     return expected_daily_hospit
 
+
+# %%
+
 def compare_results(expected_daily_hospit, phi, ll):
     # compare observed hospitalizations to model results
     # likelihood of the data wrt. to the model
@@ -141,16 +188,23 @@ def compare_results(expected_daily_hospit, phi, ll):
         p = 1 / (1 + expected_daily_hospit[i] / phi)
         if p == 1:
             p = p.clone() - torch.tensor(2.225e-5)
-        dist = torch.distributions.negative_binomial.NegativeBinomial(phi + torch.tensor(2.225e-5), p)
+        if p < 2.225e-5:
+            p = p.clone() + torch.tensor(2.225e-5)
+        dist = torch.distributions.negative_binomial.NegativeBinomial(phi, p - torch.tensor(2.225e-5))
         ll += dist.log_prob(observed_daily_hospit[i])
     return ll
 
+
+# %%
+
 def forward_pass():
     # Initialize y
-    y = trunc_exponential(tau, 1000)
+    tau = bij_transform(tau_prime, lower=0, upper=200)
+    y = torch.distributions.exponential.Exponential(1 / tau).rsample()
     R0 = bij_transform(R0_prime, lower=2, upper=5)
     phi = bij_transform(phi_prime, lower=1, upper=50)
     alpha = bij_transform(alpha_prime, lower=0, upper=0.05)
+    sigma = bij_transform(sigma_prime, lower=0, upper=0.5)
 
     # Calculate prior loss
     ll = calc_prior_loss(tau, phi, R0, alpha, sigma)
@@ -169,107 +223,52 @@ def forward_pass():
 
     # Compare observed hospitalizations to model results
     ll = compare_results(expected_daily_hospit, phi, ll)
-    return expected_daily_hospit, Rt, ll, R0, phi, alpha
+
+    return expected_daily_hospit, Rt, ll, tau, R0, phi, alpha, sigma
 
 
-
-# Initialization
-
-
-cero = torch.tensor(0., requires_grad=False, device=device, dtype=dtype)
-num_impute = 6
-observed_daily_hospit = torch.tensor(data.hospit, requires_grad=False, device=device, dtype=dtype)
-pi = torch.tensor(data.delay_distr, requires_grad=False, device=device, dtype=dtype)
-serial_interval = torch.tensor(data.serial_interval, requires_grad=False, device=device, dtype=dtype)
-population = torch.tensor(5793636, requires_grad=False, device=device, dtype=dtype)
-num_observations = len(observed_daily_hospit)
-
-
-## Initialize latent variables/parameters
-
-
-tau = torch.tensor(np.random.exponential(1 / 0.03), requires_grad=True, device=device, dtype=dtype)
-sigma = torch.tensor(truncnorm.rvs((0 - 0.1) / 0.3, (0.5 - 0.1) / 0.3, loc=0.1, scale=0.3), requires_grad=True,
-                     device=device,
-                     dtype=dtype)  # positive, tricky, gamma or inverse gamma, log normal  --> try something out, large sigma--> prone to overfitting # standart deviation of random walk step
-
-phi_prime = torch.tensor(truncnorm.rvs((0 - 25) / 10, (np.inf - 25) / 10, loc=25, scale=10), requires_grad=True,
-                   device=device,
-                   dtype=dtype)  # has to be positive, between 0-50 --> uniform # dispersion (shape) parameter for observations
-alpha_prime = torch.tensor(truncnorm.rvs((0 - 1 / 100) / 1 / 100, (5 / 100 - 1 / 100) / 1 / 100, loc=1 / 100, scale=1 / 100),
-                     requires_grad=True, device=device,
-                     dtype=dtype)  # uniform distribution between (0-5%) # probability to get hospitalized
-R0_prime = torch.rand(1, dtype=dtype, device=device, requires_grad=True)
-
-epsilon_t = torch.zeros(num_observations, device=device)
-epsilon_t[0] = torch.distributions.Normal(cero, sigma.detach()).rsample()
-for t in range(1, num_observations):
-    epsilon_t[t] = torch.distributions.Normal(epsilon_t[t - 1].detach(), sigma.detach()).rsample()
-epsilon_t.requires_grad_(True)
-
-# %%
-
-dist_tau_t = distributions.exponential.Exponential(torch.tensor([1 / 0.03], device=device))
-# tau_t = dist_tau_t.sample()
-
-dist_y = distributions.exponential.Exponential(tau)
-# y = dist_y.sample()
-
-dist_phi = distributions.normal.Normal(loc=torch.tensor([25], device=device), scale=torch.tensor([10], device=device))
-# phi = dist_phi.sample()
-
-dist_R0 = distributions.normal.Normal(loc=torch.tensor([3.6], device=device), scale=torch.tensor([0.8], device=device))
-# R0 = dist_R0.sample()
-
-dist_alpha = distributions.normal.Normal(loc=torch.tensor([0.01], device=device),
-                                         scale=torch.tensor([0.01], device=device))
-# alpha = dist_alpha.sample()
-
-dist_sigma = distributions.normal.Normal(loc=torch.tensor([0.1], device=device),
-                                         scale=torch.tensor([0.3], device=device))
-
+# %% md
 
 # Optimization
 
 # %%
 
-learning_rate = 1e-3
+learning_rate = 1e-4
 epochs = 1000
 complete_time = time.time()
 
 for k in range(epochs):
     start_time = time.time()
-    #decay = (1 - (k / (epochs * 1e5))) ** 2
-    #learning_rate = learning_rate * decay
+    decay = (1 - (k / (epochs * 1e5))) ** 2
+    learning_rate = learning_rate * decay
 
     # forward pass - calculate expected_daily_hospit
-    expected_daily_hospit, Rt, ll, R0, phi, alpha = forward_pass()
+    expected_daily_hospit, Rt, ll, tau, R0, phi, alpha, sigma = forward_pass()
 
     # backward pass
-    loss = -ll
+    loss = ll
     loss.backward()
 
-    if k % 1 == 0:
-        print(f'Time Step: {k}|| Loss: {loss}, RO:{R0}  R0":{R0_prime}, grad: {R0_prime.grad}, alpha": {alpha_prime} grad: {alpha_prime.grad}, phi": {phi_prime} grad: {phi_prime.grad}, sigma: {sigma} grad {sigma.grad}, epsilon_t.mean: {epsilon_t.mean()} grad.mean {epsilon_t.grad.mean()}')
-        #print(f'Time Step: {k}||R0":{R0_prime}, grad: {R0_prime.grad}, R0:{R0}')
-
+    if k % 5 == 0:
+        print(
+            f'Time Step: {k}|| Loss: {loss},  R0:{R0}, grad: {R0_prime.grad}, alpha: {alpha} grad: {alpha_prime.grad}, phi: {phi} grad: {phi_prime.grad}, sigma: {sigma} grad {sigma_prime.grad}, epsilon_t.mean: {epsilon_t.mean()} grad.mean {epsilon_t.grad.mean()}')
+        print("This Run:  %s seconds" % (time.time() - start_time))
     with torch.no_grad():  # this part is SGD. can also replace with loss.step
-        tau -= learning_rate * tau.grad * 1e-10
+        tau_prime -= learning_rate * tau_prime.grad
         phi_prime -= learning_rate * phi_prime.grad
-        # R0_prime -= learning_rate * R0_prime.grad
         R0_prime -= learning_rate * R0_prime.grad
         alpha_prime -= learning_rate * alpha_prime.grad
-        sigma -= learning_rate * sigma.grad * 1e-10
+        sigma_prime -= learning_rate * sigma_prime.grad
         epsilon_t -= learning_rate * epsilon_t.grad
 
-        tau.grad = None
+        tau_prime.grad = None
         phi_prime.grad = None
         R0_prime.grad = None
         alpha_prime.grad = None
-        sigma.grad = None
+        sigma_prime.grad = None
         epsilon_t.grad = None
 
-    if k % 20 == 0:
+    if k % 10 == 0:
         plt.plot(expected_daily_hospit.cpu().detach().numpy(), label='expected_daily_hospit')
         plt.plot(observed_daily_hospit.cpu().detach().numpy(), label='observed_daily_hospit')
         plt.legend()
@@ -277,18 +276,6 @@ for k in range(epochs):
 
 print("Complete Run:  %s seconds" % (time.time() - complete_time))
 
-# use to save toy data
+# %%
 
-'''
-toy_data = {'observed_daily': expected_daily_hospit,
-            'R0_prime': R0_prime,
-            'alpha_prime': alpha_prime,
-            'phi_prime': phi_prime,
-            'sigma': sigma,
-            'epsilon_t_mean':epsilon_t.mean()
-            }
 
-pickle.dump(toy_data, open("toy.pickle", "wb")) 
-
-print("Complete Run:  %s seconds" % (time.time() - complete_time))
-'''
